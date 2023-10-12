1e1be813e02356b91b62323a43c5d681d59c8103
defford <defford@gmail.com>
2023-10-12 20:00:50 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+// Timer Variables & Functions (from previous code)
 let interval;
-let timeLeft = 25 * 60; // 25 minutes in seconds
+let timeLeft = 25 * 60;
 const timerElement = document.getElementById("timer");
+const beep = document.getElementById("beep");
 
-const updateDisplay = () => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    timerElement.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-};
-
-const startTimer = () => {
-    interval = setInterval(() => {
-        timeLeft--;
-        updateDisplay();
-        if (timeLeft <= 0) {
-            clearInterval(interval);
-            alert("Time's up!");
-        }
-    }, 1000);
-};
-
-const stopTimer = () => {
-    clearInterval(interval);
-};
-
-const resetTimer = () => {
-    stopTimer();
-    timeLeft = 25 * 60; // Reset to 25 minutes
-    updateDisplay();
-};
-
-// Initialize the timer display
+const updateDisplay = /* ... */;
+const startTimer = /* ... */;
+const stopTimer = /* ... */;
+const resetTimer = /* ... */;
+const playBeep = /* ... */;
 updateDisplay();
+
+// To-Do Variables & Functions
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+const addTask = () => {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = document.createElement("li");
+    newTask.textContent = taskText;
+
+    // Add delete button to each task
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = () => {
+        taskList.removeChild(newTask);
+    };
+
+    // Append the delete button and the task to the list
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+
+    // Clear input
+    taskInput.value = "";
+};
+
+// Allow adding tasks with "Enter" key
+taskInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
